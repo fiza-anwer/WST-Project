e7f78153b788afa252cab52ba405fee7f9f3422b
Fiza Anwer <anwerfiza5@gmail.com>
2025-06-03 00:25:27 +0500

    4

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const http = require('http');
 const { Server } = require('socket.io');
 require('dotenv').config();
+const serverless = require('serverless-http');
 
 const app = express();
 const server = http.createServer(app);
@@ -62,4 +63,4 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
-module.exports = { io };
+module.exports = serverless(app);
